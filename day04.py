f23b4648fca979afc9427639783647e0aecbc612
kimdonghyuk <144420387+hahagoodgood@users.noreply.github.com>
2025-02-22 00:23:09 +0900

    v1.2 day04 task

--- day04.py
+++ day04.py
@@ -1,37 +1,40 @@
-import random
-drinks_foods = {"위스키": "초콜릿", "와인": "치즈", "소주": "삽겹살", "고량주": "양꼬치"}
-# print(drinks_foods)
-# print(drinks_foods.pop("고량주"))
-# print(drinks_foods)
+# SOLID
+# Open Closed Principle : 개방 폐쇄 원칙 (확장에는 열려 있고 수정에는 닫혀있는 원칙)
+import time
 
-#del drinks_foods["위스키"]
-#drinks_foods["사케"] = "광어회"
-japan_drinks_foods = {"사케": "광어회", "위스키": "낙곱새"}
-drinks_foods.update(japan_drinks_foods)
+def description_decorator(func):
+    def wrapper(*args):
+        print(func.__name__)
+        print(func.__doc__)
+        r = func(*args)
+        return r
+    return wrapper
 
-#drink = input(drinks_foods.keys())
-drinks_foods_keys = list(drinks_foods)
-# print(drinks_foods_keys)
-# #print(drinks_foods_keys.pop(0))
-# print(drinks_foods_keys.remove("위스키"))
-# print(drinks_foods_keys)
-#print(random.choice(drinks_foods_keys))
+def time_decorator(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f'실행시간 : {e - s}초')
+        return r
+    return wrapper
 
-while True:
-    menu = input(f'다음 술중에 고르세요.\n1) {drinks_foods_keys[0]}   2) {drinks_foods_keys[1]}   3) {drinks_foods_keys[2]}   4) {drinks_foods_keys[3]}   5) {drinks_foods_keys[4]}   6) 아무거나   7) 종료 : ')
-    if menu == '1':
-        print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]} 입니다')
-    elif menu == '2':
-        print(f'{drinks_foods_keys[1]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[1]]} 입니다')
-    elif menu == '3':
-        print(f'{drinks_foods_keys[2]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[2]]} 입니다')
-    elif menu == '4':
-        print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]} 입니다')
-    elif menu == '5':
-        print(f'{drinks_foods_keys[4]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[4]]} 입니다')
-    elif menu == '6':
-        random_drink = random.choice(drinks_foods_keys)
-        print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]} 입니다')
-    elif menu == '7':
-        print(f'다음에 또 오세요')
-        break
+@time_decorator
+@description_decorator
+def factorial_repetition(n) -> int:
+    """
+        factorial function by loop
+        :param n:
+        :return: results of factorial operation
+    """
+    result = 1
+    for i in range(2, n+1):
+        result = result * i
+    return result
+
+
+number = int(input())
+ft = time_decorator(factorial_repetition)
+print(f"{number}! = {ft(number)}")
+number = int(input())
+print(f"{number}! = {factorial_repetition(number)}")
